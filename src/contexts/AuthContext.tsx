c4c449a5d47be2f1651da49f5acc3ee9d9bfc0f2
Davi <dabisilvaond@gmail.com>
2021-05-12 10:41:33 -0300

    Saving user data, Saving tokens in cookies, Retrieving authentication status, Performing token refresh, Request queue in Axios and Performing automatic logout

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,13 @@
-import { createContext, ReactNode, useContext, useState } from "react";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { setCookie, parseCookies, destroyCookie } from "nookies";
 import Router from "next/router";
-import api from "../services/api";
+import { api } from "../services/api";
 
 type User = {
   email: string;
@@ -25,20 +32,58 @@ type AuthProviderProps = {
 
 const AuthContext = createContext({} as AuthContextData);
 
+export function signOut() {
+  destroyCookie(undefined, "nextauth.token");
+  destroyCookie(undefined, "nextauth.refreshToken");
+
+  Router.push("/");
+}
+
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
+
+  useEffect(() => {
+    const { "nextauth.token": token } = parseCookies();
+    if (token) {
+      api
+        .get("me")
+        .then((response) => {
+          const { permissions, roles, email } = response.data;
+          setUser({
+            email,
+            permissions,
+            roles,
+          });
+        })
+        .catch(() => {
+          signOut();
+        });
+    }
+  }, []);
 
   async function signIn({ email, password }: SignInCredentials) {
     await api
       .post("sessions", { email, password })
       .then((response) => {
-        const { permissions, roles } = response.data;
+        const { permissions, roles, token, refreshToken } = response.data;
+
+        setCookie(undefined, "nextauth.token", token, {
+          maxAge: 60 * 60 * 24 * 30, //30 dias
+          path: "/",
+        });
+        setCookie(undefined, "nextauth.refreshToken", refreshToken, {
+          maxAge: 60 * 60 * 24 * 30, //30 dias
+          path: "/",
+        });
         setUser({
           email,
           permissions,
           roles,
         });
+
+        api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
         Router.push("/dashboard");
       })
       .catch((err) => console.log(err.data));
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
 import { useAuth } from "../contexts/AuthContext";
+import { api } from "../services/api";
 
 export default function Dashboard() {
   const { user } = useAuth();
 
+  useEffect(() => {
+    api
+      .get("me")
+      .then((response) => console.log(response))
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div>
       <h1>Dashboard: {user?.email}</h1>
